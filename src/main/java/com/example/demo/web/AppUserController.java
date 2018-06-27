9b8bf54cf8d6d8036f414d363c04bdbd149e9e05
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-06-27 17:37:46 +0200

    Append global method security

--- src/main/java/com/example/demo/web/AppUserController.java
+++ src/main/java/com/example/demo/web/AppUserController.java
@@ -8,9 +8,12 @@ package com.example.demo.web;
 import com.example.demo.jpa.model.AppUser;
 import com.example.demo.service.UserApiServiceInterface;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,12 +30,16 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 @RequestMapping("/users")
 public class AppUserController {
 
+	private static final Logger LOG = Logger.getLogger(AppUserController.class.getName());
+	
 	@Autowired
 	@Qualifier("jpaUserApiService")
 	private UserApiServiceInterface userApiService;
 	
 	@RequestMapping(value = "/getAll", method = GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	@Secured({"ROLE_USER"})
 	public List<AppUser> getAll() {
+		LOG.log(Level.WARNING, "getAll() method invocation");
 		return userApiService.getAllUsers();
 	}
 
