a5bd1c090587f2cd732a347efe90501178974473
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-06-27 10:18:45 +0200

    Create user functionality rest controller and test

---
+++ src/main/java/com/example/demo/web/AppUserController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo.web;
+
+import com.example.demo.jpa.model.AppUser;
+import com.example.demo.service.UserApiServiceInterface;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+/**
+ *
+ * @author dnikiforov
+ */
+@RestController
+@RequestMapping("/users")
+public class AppUserController {
+
+	@Autowired
+	@Qualifier("jpaUserApiService")
+	private UserApiServiceInterface userApiService;
+	
+	@RequestMapping(value = "/getAll", method = GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+	public List<AppUser> getAll() {
+		return userApiService.getAllUsers();
+	}
+
+}
---
+++ src/test/java/com/example/demo/AppUserControllerTests.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import com.example.demo.jpa.model.AppUser;
+import com.example.demo.util.CommonHelper;
+import com.example.demo.web.AppUserController;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppUserControllerTests {
+
+	@Autowired
+	private AppUserController appUserController;
+
+	@Test
+	public void testGetAllUsers() throws IOException {
+		final List<AppUser> all = appUserController.getAll();
+		final AppUser[] array = CommonHelper.transformJsonToObject("defaultUserSet.json", AppUser[].class);
+		final List<AppUser> fromFile = Arrays.stream(array).collect(Collectors.toList());
+		Collections.sort(all);
+		Collections.sort(fromFile);
+		assertEquals(all, fromFile);
+	}
+
+}
