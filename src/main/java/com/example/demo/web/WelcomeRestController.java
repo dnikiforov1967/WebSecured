92aaa4c1eeaac61610c032db751a4b0d2f39fac2
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-06-27 17:08:09 +0200

    Append method to setup authentication in SecurityContext

--- src/main/java/com/example/demo/web/WelcomeRestController.java
+++ src/main/java/com/example/demo/web/WelcomeRestController.java
@@ -5,24 +5,40 @@
  */
 package com.example.demo.web;
 
-
-import org.springframework.web.bind.annotation.PathVariable;
+import com.example.demo.security.resource.LoginResource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  *
  * @author dnikiforov
  */
 @RestController
-@RequestMapping("/welcome")
+@RequestMapping("/security/login")
 public class WelcomeRestController {
 
-	@RequestMapping(value = "/{id}", method = GET)
-	public String get(@PathVariable String id) {
-		return id;
+	@Autowired
+	private AuthenticationManager authenticationManager;
+
+	@RequestMapping(method = RequestMethod.POST, produces = {APPLICATION_JSON_VALUE}, consumes = {APPLICATION_JSON_VALUE})
+	public LoginResource login(@RequestBody LoginResource loginResource) {
+		UsernamePasswordAuthenticationToken token
+				= new UsernamePasswordAuthenticationToken(loginResource.getUsername(), loginResource.getPassword());
+
+		Authentication auth = authenticationManager.authenticate(token);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		return loginResource;
 	}
 
 }
