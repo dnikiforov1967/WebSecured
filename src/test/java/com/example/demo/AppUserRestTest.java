a9cdca9de432f9d23d0c2bd30cebd955c141a0f7
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-06-28 11:46:07 +0200

    Implement web mock test for user controller

---
+++ src/test/java/com/example/demo/AppUserRestTest.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.demo;
+
+import com.example.demo.jpa.model.AppRole;
+import com.example.demo.jpa.model.AppUser;
+import com.example.demo.jpa.model.AppUserRole;
+import com.example.demo.jpa.model.AppUserRoleKey;
+import com.example.demo.service.UserApiServiceInterface;
+import com.example.demo.util.CommonHelper;
+import com.example.demo.web.AppUserController;
+import com.example.demo.web.resource.AppUserResource;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Mocked integration test
+ *
+ * @author dnikiforov
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(AppUserController.class)
+@WithMockUser(username = "user1", password = "1111", roles = {"USER"})
+public class AppUserRestTest {
+
+	private static AppUser user;
+	private static String jsonView;
+
+	@MockBean(name = "jpaUserApiService")
+	private UserApiServiceInterface userApiService;
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	public AppUserRestTest() {
+	}
+
+	@BeforeClass
+	public static void setUpClass() throws IOException {
+		user = new AppUser("dmitry", "Qahjasha");
+		user.setAppUserRoles(Collections.singleton(new AppUserRole(new AppUserRoleKey(user, AppRole.ROLE_ADMIN))));
+		final List<AppUserResource> singletonList = Collections.singletonList(new AppUserResource(user));
+		jsonView = CommonHelper.transformObjectToJson(singletonList);
+	}
+
+	@AfterClass
+	public static void tearDownClass() {
+	}
+
+	@Before
+	public void setUp() {
+		Mockito.when(userApiService.getAllUsers()).thenReturn(Collections.singletonList(user));
+	}
+
+	@After
+	public void tearDown() {
+	}
+
+	@Test
+	public void testGetAllUsers() throws Exception {
+		mockMvc
+				.perform(
+						get("/users/getAll")
+								.accept(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(status().isOk())
+				.andExpect(content().json(jsonView));
+	}
+
+}
