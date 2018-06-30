d6b64bd5f5cab0b8f6c010715e0d262bf80c9c9d
dnikiforov1967 <dnikiforov1967@gmail.com>
2018-06-30 15:09:04 +0200

    Implement JTA transactions into app

--- src/main/java/com/example/demo/config/DatabaseConfiguration.java
+++ src/main/java/com/example/demo/config/DatabaseConfiguration.java
@@ -5,30 +5,94 @@
  */
 package com.example.demo.config;
 
+import com.atomikos.icatch.jta.UserTransactionImp;
+import com.atomikos.icatch.jta.UserTransactionManager;
 import com.atomikos.jdbc.AtomikosDataSourceBean;
+import com.example.demo.jpa.model.AppUser;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import javax.sql.DataSource;
+import javax.transaction.TransactionManager;
+import javax.transaction.UserTransaction;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.jta.JtaTransactionManager;
 
 /**
  *
  * @author dima
  */
 @Configuration
+@EnableTransactionManagement
 public class DatabaseConfiguration {
 
+    @Value("${atomikos.datasource.url}")
+    private String urlDatabase;
+    @Value("${atomikos.datasource.xa-class}")
+    private String xaClass;
+    @Value("${atomikos.datasource.username}")
+    private String username;
+    @Value("${atomikos.datasource.password}")
+    private String password;     
+    
+
     @Bean
-    public DataSource getDataSource() {
+    public DataSource dataSource() {
         AtomikosDataSourceBean atomikosDataSourceBean = new AtomikosDataSourceBean();
-        atomikosDataSourceBean.setXaDataSourceClassName("org.h2.jdbcx.JdbcDataSource");
+        atomikosDataSourceBean.setXaDataSourceClassName(xaClass);
         Properties properties = new Properties();
-        properties.put("user", "sa");
-        properties.put("password", "sa");
-        properties.put("url", "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        properties.put("user", username);
+        properties.put("password", password);
+        properties.put("url", urlDatabase);
         atomikosDataSourceBean.setXaProperties(properties);
         atomikosDataSourceBean.setUniqueResourceName("mainDataSourceResource");
         return atomikosDataSourceBean;
+    }
+
+    @Bean
+    @DependsOn({"transactionManager"})
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(
+            EntityManagerFactoryBuilder builder,
+            DataSource dataSource) {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("javax.persistence.schema-generation.database.action", "drop-and-create");
+        properties.put("hibernate.transaction.jta.platform", "com.example.demo.config.CustomJtaPlatform");
+        return builder
+                .dataSource(dataSource)
+                .jta(true)
+                .persistenceUnit("globalPU")
+                .packages(AppUser.class)
+                .properties(properties)
+                .build();
+    }
+
+    @Bean(name = "userTransaction")
+    public UserTransaction userTransaction() throws Throwable {
+        UserTransactionImp userTransactionImp = new UserTransactionImp();
+        userTransactionImp.setTransactionTimeout(300);
+        return userTransactionImp;
+    }
+
+    @Bean(name = "atomikosTransactionManager", initMethod = "init", destroyMethod = "close")
+    public TransactionManager atomikosTransactionManager() throws Throwable {
+        UserTransactionManager userTransactionManager = new UserTransactionManager();
+        userTransactionManager.setForceShutdown(false);
+        return userTransactionManager;
+    }
+
+    @Bean(name = "transactionManager")
+    @DependsOn({"userTransaction", "atomikosTransactionManager"})
+    public PlatformTransactionManager transactionManager(UserTransaction userTransaction, TransactionManager atomikosTransactionManager) throws Throwable {
+        CustomJtaPlatform.setUserTransaction(userTransaction);
+        CustomJtaPlatform.setTransactionManager(atomikosTransactionManager);
+        return new JtaTransactionManager(userTransaction, atomikosTransactionManager);
     }
 
 }
